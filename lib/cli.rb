c815c1dc4195dac5d40e68889536dbcd35f7132a
Katrina Pritchard <pritckat@gmail.com>
2018-08-30 16:28:10 +0000

    updated initialize menu for cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,6 +1,10 @@
 class CLI
 
   def initialize
-    puts "Welcome to Overwatch World Cup Team Overview 2018"
+    puts "Welcome to Overwatch World Cup Team Overview 2018!"
+    puts "To see a list of teams, type 'teams'."
+    puts "To see a list of players, type 'players'."
+    puts "To exit, type exit."
+    puts "What would you like to do?"
   end
 end
