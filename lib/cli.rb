c497953d1973bc54521304e774d58640173b54cb
Katrina Pritchard <pritckat@gmail.com>
2018-08-30 18:22:44 +0000

    added more to class CLI initialization, added if statements and until

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,7 +4,21 @@ class CLI
     puts "Welcome to Overwatch World Cup Team Overview 2018!"
     puts "To see a list of teams, type 'teams'."
     puts "To see a list of players, type 'players'."
+    puts "To see a list of characters, type 'characters'."
     puts "To exit, type exit."
     puts "What would you like to do?"
+    input = gets.strip
+    if input == 'teams'
+
+    elsif input == 'players'
+
+    elsif input == 'characters'
+    end
+    until input == 'exit'
+      input = gets.strip
+    end
+
   end
+
+
 end
