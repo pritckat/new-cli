fdfb30cb5ed9fac29e007a8fbc6027c57d52efec
Katrina Pritchard <pritckat@gmail.com>
2018-08-31 00:14:11 +0000

    scraper for players

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,19 +6,20 @@ class Scraper
 
   def self.scrape_team_page
 
-    doc = Nokogiri::HTML(open("https://liquipedia.net/overwatch/Overwatch_World_Cup/2018/Rosters"))
-    #teams = []
-    doc.css(".template-box").each do |teamcard|
+    @doc = Nokogiri::HTML(open("https://liquipedia.net/overwatch/Overwatch_World_Cup/2018/Rosters"))
+    @doc.css(".template-box").each do |teamcard|
       team_url = teamcard.css("b a").attribute("href").value
       team_name = teamcard.css("b a").attribute("title").value
       team = Team.new(team_name, team_url)
       Team.all << team
-      #teams << {name: team_name}
+    end
+    roster = []
+    removable = @doc.css(".team-template-team-part")
+    removable.remove
+    @doc.css(".teamcard-inner table tbody tr td").each do |t|
+      player = t.css("a").text
+      roster << player
     end
     binding.pry
   end
 end
-
-#teamcard: doc.css(".template-box")
-#team name: doc.css("b a").attribute("title").value
-#team url: doc.css("b a").attribute("href").value
